0c83f0c6e36cc585a9d4830e110618b63a12230c
Aleksandar Yanev <a.yanev90@gmail.com>
2016-11-10 15:45:04 +0200

    Miracle Pill App

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Aleksandar Yanev on 11/7/16.
+//  Copyright © 2016 AleksandarYanev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func stateBtnPressed(_ sender: Any) {
+    
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+}
+    
+    
+
+
+
+
+
